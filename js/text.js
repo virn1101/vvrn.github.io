f9f1a3e08a16f42de8a05356c811e6b9e2a9e47f
virn1101 <43517224+virn1101@users.noreply.github.com>
2019-10-06 00:21:45 +0530

    Add files via upload

---
+++ js/text.js
@@ -0,0 +1,12 @@
+$('#text').lettering()
+
+/*Contact
+
+Email: hugo@darbybrown.com
+Twitter: https://twitter.com/darbybrown
+Website: Coming Soon
+
+Fork the mixin on github:
+https://github.com/DarbyBrown/Scss-3D-Text-Mixin
+
+*/
